3da6b4026695019877d1cd7c4389d0c4749b560a
avinash-v2solutions <avinash.hediggond@v2solutions.com>
2025-02-05 17:36:35 +0530

    RestAPI Testing

--- tests/RestAPI.test.ts
+++ tests/RestAPI.test.ts
@@ -73,3 +73,101 @@ test("DELETE User", async({request})=>{
 
 
 })
+
+test(" GET Single User", async ({request})=>{
+
+   const response =  await request.get("https://reqres.in/api/users/2")
+   console.log(await response.json())
+   expect(response.status()).toBe(200)
+
+})
+
+test(" GET Single User Not Found", async ({request})=>{
+
+   const response =  await request.get("https://reqres.in/api/users/23")
+   console.log(await response.json())
+   expect(response.status()).toBe(404)
+
+})
+
+test(" GET List Resource", async ({request})=>{
+
+   const response =  await request.get("https://reqres.in/api/unknown")
+   console.log(await response.json())
+   expect(response.status()).toBe(200)
+
+})
+
+test(" GET Sinle Resource", async ({request})=>{
+
+   const response =  await request.get("https://reqres.in/api/unknown/2")
+   console.log(await response.json())
+   expect(response.status()).toBe(200)
+
+})
+
+test(" GET Sinle Resource Not Found", async ({request})=>{
+
+   const response =  await request.get("https://reqres.in/api/unknown/23")
+   console.log(await response.json())
+   expect(response.status()).toBe(404)
+
+})
+
+test(" GET Sinle Resource Not Found", async ({request})=>{
+
+   const response =  await request.get("https://reqres.in/api/unknown/23")
+   console.log(await response.json())
+   expect(response.status()).toBe(404)
+
+})
+
+test ("PATCH Update User", async({request})=>{
+
+   const response = await request.put("https://reqres.in/api/users/2",{
+        data:{
+            
+    "name": "morpheus",
+    "job": "zion resident"
+        },
+    headers:{
+
+            "Accept": "application/json"
+          }
+
+    })
+
+    console.log(await response.json())
+
+expect(response.status()).toBe(200)
+
+var res =await response.json()
+var update = await res.updatedAt
+console.log('Updated the User : '+update)
+
+})
+
+https://reqres.in/api/register
+test("POST Register Successfull", async ({request})=>{
+
+   const response = await request.post("https://reqres.in/api/users",{
+                                                      data:{
+                                                             "email": "eve.holt@reqres.in",
+                                                              "password": "pistol"
+                                                          },
+                                                      headers:{
+   
+                                                              "Accept": "application/json"
+                                                            }
+   })
+   
+   console.log(await response.json())
+   
+   expect(response.status()).toBe(201)
+   
+   var res =await response.json()
+   var userID = await res.id
+   console.log('Created User Id is : '+userID)
+
+   var token =await res.token
+   console.log('Created User Id is : '+token )
