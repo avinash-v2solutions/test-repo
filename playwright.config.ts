c2bc53dd352f3ad389139c06e1bde57173265c24
avinash-v2solutions <avinash.hediggond@v2solutions.com>
2025-02-05 17:39:44 +0530

    RestAPI Test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config : PlaywrightTestConfig = {
-  testMatch : ["SauceDemo.test.ts"],
+  testMatch : ["RestAPI.test.ts"],
 
   retries:6,
 
