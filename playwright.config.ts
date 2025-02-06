b9698a6d313d47ac5a59ed1e73d67f2df1aa674f
avinash-v2solutions <avinash.hediggond@v2solutions.com>
2025-02-06 11:00:40 +0530

    Mock Network Request

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config : PlaywrightTestConfig = {
-  testMatch : ["Ecommerce.test.ts"],
+  testMatch : ["MockNetworkRequest.test.ts"],
 
   retries:6,
 
